79ed3e6347af9e67e13357ff87ebb3360755a25f
egradman <eric.gradman@red6ar.com>
2024-03-31 09:04:50 -0700

    tmux vim navigator

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,6 +16,23 @@ return {
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
   },
   {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  {
     "ellisonleao/gruvbox.nvim",
     priority = 1000 , config = true,
     opts = { contrast = "hard" }
