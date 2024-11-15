bdae12355b93d4661e9e71893dd62c1db6640ec2
egradman <eric@gradman.com>
2024-11-15 12:18:30 -0800

    copypaste working, more nushell

--- raycast_scripts/kitty.py
+++ raycast_scripts/kitty.py
@@ -10,25 +10,25 @@ import subprocess
 match sys.argv[1].lower():
 
     case "dmz":
-        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "dmz" --color background="#002200" -- kitten ssh -t egradman@dmz.gradman.com tmux new-session -A -s main '''
+        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "dmz" --color background="#002200" -- kitten ssh -t egradman@dmz.gradman.com zsh -l -c "tmux -u new-session -A -s main"  '''
 
     case "minecraft":
-        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "minecraft" --color background="#000022" -- kitten ssh -t root@minecraft.gradman.com tmux new-session -A -s main '''
+        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "minecraft" --color background="#000022" -- kitten ssh -t root@minecraft.gradman.com zsh -l -c "tmux -u new-session -A -s main"  '''
 
     case "homeassistant":
         cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "homeassistant" --color background="#000022" -- kitten ssh -t egradman@192.168.68.63 '''
 
     case "echt raspberry pi":
-        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "ECHT Raspberry Pi" --color background="#220000" -- kitten ssh -t ubuntu@192.168.2.130 tmux new-session -A -s main '''
+        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "ECHT Raspberry Pi" --color background="#220000" -- kitten ssh -t ubuntu@192.168.2.130 zsh -l -c "tmux -u new-session -A -s main"  '''
 
     case "acu":
-        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "ACU" --color background="#220000" -- kitten ssh -t rsadmi@192.168.2.1 tmux new-session -A -s main '''
+        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "ACU" --color background="#220000" -- kitten ssh -t rsadmi@192.168.2.1 zsh -l -c "tmux -u new-session -A -s main"  '''
 
     case "credence":
-        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "credence" --color background="#220000" -- kitten ssh -t rsadmi@192.168.8.8 tmux new-session -A -s main '''
+        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "credence" --color background="#220000" -- kitten ssh -t rsadmi@192.168.8.8 zsh -l -c "tmux -u new-session -A -s main"  '''
 
     case "eyecap":
-        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "eyecap" --color background="#220000" -- kitten ssh -t rsadmi@192.168.2.2 tmux new-session -A -s main '''
+        cmd = '''kitten @ launch --to=unix:`ls /tmp/kitty-* | tail -1` --type=tab --tab-title "eyecap" --color background="#220000" -- kitten ssh -t rsadmi@172.16.186.128 zsh -l -c "tmux -u new-session -A -s main"  '''
 
 
 print(cmd)
