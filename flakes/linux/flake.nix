edc058db39023a8d5b0f89e215bff9b6ccb620e7
egradman <eric@gradman.com>
2025-01-22 10:55:15 -0800

    latest

--- flakes/linux/flake.nix
+++ flakes/linux/flake.nix
@@ -9,6 +9,7 @@
     let pkgs = import nixpkgs {
         system = "x86_64-linux";
         overlays = [nixgl.overlay];
+        config = { allowUnfree = true; };
       };
     in {
       packages."x86_64-linux".default = pkgs.buildEnv {
@@ -18,21 +19,9 @@
           pkgs.ulauncher
           pkgs.gnome-tweaks
           pkgs.zsh
+          "pkgs.google-chrome-stable"
         ];
       };
     };
-
-
-#    packages."x86_64-linux".default = let
-#      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-#    in
-# pkgs.buildEnv {
-#      name = "home-packages";
-#      paths = import ./tools.nix {pkgs=pkgs;} ++ [
-#	pkgs.nixGL
-#	pkgs.ulauncher
-#      ];
-#    };
-
 }
 
